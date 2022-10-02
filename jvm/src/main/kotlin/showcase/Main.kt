86808248fa730339501ddf556c0a4ea56eee21de
David Wursteisen <david.wursteisen+git@gmail.com>
2022-10-02 23:56:48 +0200

    Update JVM configuration

--- jvm/src/main/kotlin/showcase/Main.kt
+++ jvm/src/main/kotlin/showcase/Main.kt
@@ -5,6 +5,9 @@ import com.github.dwursteisen.minigdx.GameConfiguration
 import com.github.dwursteisen.minigdx.GameScreenConfiguration
 import com.github.dwursteisen.minigdx.Window
 import com.github.minigdx.showcase.Particles
+import com.github.minigdx.showcase.dance.DanceGame
+import com.github.minigdx.showcase.imguilight.ImGUILight
+import com.github.minigdx.showcase.physic.SatTank
 import com.github.minigdx.showcase.text.DebugText
 
 class Main {
@@ -15,14 +18,15 @@ class Main {
         fun main(vararg args: String) {
             GameApplicationBuilder(
                 gameFactory = {
-                    Particles(it)
+                    DanceGame(it)
                 },
                 gameConfigurationFactory = {
                     GameConfiguration(
                         "MiniGDX Showcase",
                         gameScreenConfiguration = GameScreenConfiguration.WithRatio(16f / 9f),
-                        debug = true,
-                        window = Window(1000, 1000, "minigdx")
+                        debug = false,
+                        jointLimit = 100,
+                        window = Window(800, 450, "minigdx")
                     )
 
                 }
